5a6e630783a40df394fde5f5ae6c4d5a6af729ac
DESKTOP-SN9KF5O\Strawberry <berichandev@gmail.com>
2023-04-20 15:07:23 +0100

    Update pointers for 1.3.0

--- SysBot.Pokemon/SV/Vision/PokeDataOffsetsSV.cs
+++ SysBot.Pokemon/SV/Vision/PokeDataOffsetsSV.cs
@@ -9,22 +9,22 @@ namespace SysBot.Pokemon
         public const string ScarletID = "0100A3D008C5C000";
         public const string VioletID = "01008F6008C5E000";
 
-        public static IReadOnlyList<long> BoxStartPokemonPointer { get; } = new long[] { 0x44CCA68, 0x110, 0x9B0, 0x00 };
-        public static IReadOnlyList<long> MyStatusPointer { get; } = new long[] { 0x44A98C8, 0x100, 0x40 };
+        public static IReadOnlyList<long> BoxStartPokemonPointer { get; } = new long[] { 0x44E2F48, 0x110, 0x9B0, 0x00 };
+        public static IReadOnlyList<long> MyStatusPointer { get; } = new long[] { 0x44BFBA8, 0x100, 0x40 };
 
-        public static IReadOnlyList<long> LinkTradePartnerPokemonPointer { get; } = new long[] { 0x44CCB50, 0x120, 0x1C8, 0xB8, 0x10, 0x30, 0x00 };
-        public static IReadOnlyList<long> LinkTradePartnerNameSlot1Pointer { get; } = new long[] { 0x44CCBB0, 0x28, 0xB0, 0x00 };
-        public static IReadOnlyList<long> LinkTradePartnerNameSlot2Pointer { get; } = new long[] { 0x44CCBB0, 0x28, 0xE0, 0x00 };
-        public static IReadOnlyList<long> LinkTradePartnerNIDPointer { get; } = new long[] { 0x44C7730, 0xF8, 0x08 };
+        public static IReadOnlyList<long> LinkTradePartnerPokemonPointer { get; } = new long[] { 0x44E3030, 0x120, 0x1C8, 0xB8, 0x10, 0x30, 0x00 };
+        public static IReadOnlyList<long> LinkTradePartnerNameSlot1Pointer { get; } = new long[] { 0x44E3090, 0x28, 0xB0, 0x00 };
+        public static IReadOnlyList<long> LinkTradePartnerNameSlot2Pointer { get; } = new long[] { 0x44E3090, 0x28, 0xE0, 0x00 };
+        public static IReadOnlyList<long> LinkTradePartnerNIDPointer { get; } = new long[] { 0x44DDC10, 0xF8, 0x08 };
 
-        public static IReadOnlyList<long> OverworldPointer { get; } = new long[] { 0x45187D8, 0x00, 0x388, 0x3C0, 0x00, 0x1A1C };
-        public static IReadOnlyList<long> IsSearchingPointer { get; } = new long[] { 0x44CC9B8, 0x58 }; // 0 no search, 1 search, 2 unknown (still searching)
-        public static IReadOnlyList<long> ConnectionPointer { get; } = new long[] { 0x44CCBC0, 0x10 }; // 0 not connected, 1 connected, 2 adhoc
+        public static IReadOnlyList<long> OverworldPointer { get; } = new long[] { 0x452ECB8, 0x00, 0x388, 0x3C0, 0x00, 0x1A1C };
+        public static IReadOnlyList<long> IsSearchingPointer { get; } = new long[] { 0x44E2E98, 0x58 }; // 0 no search, 1 search, 2 unknown (still searching)
+        public static IReadOnlyList<long> ConnectionPointer { get; } = new long[] { 0x44E30A0, 0x10 }; // 0 not connected, 1 connected, 2 adhoc
 
-        public static IReadOnlyList<long> KeyboardBufferPointer { get; } = new long[] { 0x44A46B8, 0x30, 0x00 };
+        public static IReadOnlyList<long> KeyboardBufferPointer { get; } = new long[] { 0x44BA968, 0x30, 0x00 };
 
 
-        public static IReadOnlyList<long> PokePortalPointer = new long[] { 0x45187D8, 0x00, 0x3C0, 0x3C0, 0x598 };
+        public static IReadOnlyList<long> PokePortalPointer = new long[] { 0x452ECB8, 0x00, 0x3C0, 0x3C0, 0x598 };
 
         public const int BoxFormatSlotSize = 0x158;
         public const int TradeFormatSlotSize = 0x148;
